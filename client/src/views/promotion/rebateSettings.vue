36a9d580a3cbb53fc1e4232348b7ae353fd7734a
hxl <18301805881@163.com>
2018-12-20 12:55:01 +0800

    #update

--- client/src/views/promotion/rebateSettings.vue
+++ client/src/views/promotion/rebateSettings.vue
@@ -3,8 +3,8 @@
 
     <!-- button -->
     <el-row style="text-align: left; margin-top:30px;">
-      <el-button type="primary" @click="dialogForm1Visible">新增</el-button>
-      <el-button type="primary" @click="dialogForm2Visible">返利经验设置</el-button>
+      <el-button type="primary" @click="dialogForm1Visible = true">新增</el-button>
+      <el-button type="primary" @click="dialogForm2Visible = true">返利经验设置</el-button>
     </el-row>
 
     <!-- table -->
@@ -39,25 +39,32 @@
 
     <!-- dialog1 新增 -->
     <el-dialog :visible.sync="dialogForm1Visible" title="新增返利等级" center>
-      <el-form :model="form1">
+      <el-form ref="ruleForm1" :model="ruleForm1" :rules="rules1">
 
-        <el-form-item :label-width="formLabel1Width" label="选择体系:">
-          <el-select v-model="form1.selectSystem" placeholder="请选择">
-            <el-option
-              v-for="item in form1.optionsSystem"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
+        <el-form-item :label-width="formLabel1Width" label="等级名称:" prop="lvName">
+          <el-input v-model="ruleForm1.lvName" autocomplete="off" placeholder="请输入"/>
         </el-form-item>
 
-        <el-form-item :label-width="formLabel1Width" label="标题:">
-          <el-input v-model="form1.title" autocomplete="off" placeholder="请输入"/>
+        <el-form-item :label-width="formLabel1Width" label="一级返点:" prop="oneLvRebatePoint">
+          <el-input v-model="ruleForm1.oneLvRebatePoint" autocomplete="off" placeholder="">
+            <template slot="append"> % </template>
+          </el-input>
         </el-form-item>
 
-        <el-form-item :label-width="formLabel1Width" label="内容">
-          <el-input v-model="form1.content" autocomplete="off" placeholder="请输入"/>
+        <el-form-item :label-width="formLabel1Width" label="二级返点:" prop="twoLvRebatePoint">
+          <el-input v-model="ruleForm1.twoLvRebatePoint" autocomplete="off" placeholder="">
+            <template slot="append"> % </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item :label-width="formLabel1Width" label="三级返点" prop="threeLvRebatePoint">
+          <el-input v-model="ruleForm1.threeLvRebatePoint" autocomplete="off" placeholder="">
+            <template slot="append"> % </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item :label-width="formLabel1Width" label="所需经验:" prop="expNeed">
+          <el-input v-model="ruleForm1.expNeed" autocomplete="off" placeholder=""/>
         </el-form-item>
 
       </el-form>
@@ -68,6 +75,30 @@
       </div>
     </el-dialog>
 
+    <!-- dialog2 返利经验设置 -->
+    <el-dialog :visible.sync="dialogForm2Visible" title="返利经验设置" center>
+      <el-form ref="ruleForm2" :model="ruleForm2" :rules="rules2">
+
+        <el-form-item :label-width="formLabel2Width" label="充值:" prop="moneyAdd">
+          <el-input v-model="ruleForm2.moneyAdd" autocomplete="off" placeholder="请输入">
+            <template slot="append"> 元 </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item :label-width="formLabel2Width" label="可获得:" prop="expAdd">
+          <el-input v-model="ruleForm2.expAdd" autocomplete="off" placeholder="">
+            <template slot="append"> 经验 </template>
+          </el-input>
+        </el-form-item>
+
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogForm2Visible = false">取消</el-button>
+        <el-button type="primary" @click="dialogForm2Visible = false">确认</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -75,6 +106,38 @@
 
 export default {
   data() {
+    // dialog1 新增; 输入检验
+    var checkRebatePoint = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('返点不能为空'))
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          // test
+          console.log('type = ' + typeof (value))
+          callback(new Error('返点设置必须为整数'))
+        } else {
+          if (value > 100 || value < 0) {
+            callback(new Error('返点设置不合理'))
+          } else {
+            callback()
+          }
+        }
+      }, 500)
+    }
+    var checkExp = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('经验设置不能为空'))
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error('经验设置必须为整数'))
+        } else {
+          callback()
+        }
+      }, 500)
+    }
+
     return {
       // tableData
       tableData: [{
@@ -104,48 +167,51 @@ export default {
 
       // dialog1 新增
       dialogForm1Visible: false,
-      form1: {
-        title: '',
-        content: '',
-
-        optionsSystem: [{
-          'label': '所有会员',
-          'value': 'allVip'
-        }, {
-          'label': '会员等级',
-          'value': 'vipLv'
-        }, {
-          'label': '单个会员',
-          'value': 'singleVip'
-        }, {
-          'label': '多个会员',
-          'value': 'multiVip'
-        }]
-      },
       formLabel1Width: '120px',
 
       // dialog2 返利经验设置
       dialogForm2Visible: false,
-      form2: {
-        title: '',
-        content: '',
+      formLabel2Width: '120px',
 
-        optionsSystem: [{
-          'label': '所有会员',
-          'value': 'allVip'
-        }, {
-          'label': '会员等级',
-          'value': 'vipLv'
-        }, {
-          'label': '单个会员',
-          'value': 'singleVip'
-        }, {
-          'label': '多个会员',
-          'value': 'multiVip'
-        }]
+      // dialog1 输入验证
+      ruleForm1: {
+        lvName: '',
+        oneLvRebatePoint: '',
+        twoLvRebatePoint: '',
+        threeLvRebatePoint: '',
+        expNeed: ''
       },
-      formLabel2Width: '120px'
+      rules1: {
+        lvName: [
+          { required: true, message: '等级名称不能为空', trigger: 'blur' }
+        ],
+        oneLvRebatePoint: [
+          { validator: checkRebatePoint, trigger: 'blur' }
+        ],
+        twoLvRebatePoint: [
+          { validator: checkRebatePoint, trigger: 'blur' }
+        ],
+        threeLvRebatePoint: [
+          { validator: checkRebatePoint, trigger: 'blur' }
+        ],
+        expNeed: [
+          { validator: checkExp, trigger: 'blur' }
+        ]
+      },
 
+      // dialog2 输入验证
+      ruleForm2: {
+        moneyAdd: '',
+        expAdd: ''
+      },
+      rules2: {
+        moneyAdd: [
+          { required: true, message: '不能为空', trigger: 'blur' }
+        ],
+        expAdd: [
+          { required: true, message: '不能为空', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
