82e702de199d7a39301599034d1839325ca6c2da
hxl <18301805881@163.com>
2018-12-25 19:14:31 +0800

    #update

--- server/test/t1.php
+++ server/test/t1.php
@@ -9,14 +9,19 @@
 require_once '../autoload.php';
 require_once '../loadConfig.php';
 
-$url = 'https://api.xsolla.com/merchant/v2/merchants/73075/token';
-$param = [
-    'svc' => 'svcAdmin',
-    'func' => 'getAdminInfo',
-    'param' => [
-        'token' => 'admin',
-        'password' => 111111
-    ]
-];
+//$url = 'https://api.xsolla.com/merchant/v2/merchants/73075/token';
+//$param = [
+//    'svc' => 'svcAdmin',
+//    'func' => 'getAdminInfo',
+//    'param' => [
+//        'token' => 'admin',
+//        'password' => 111111
+//    ]
+//];
+//
+//echo clsHttp::curlPost($url, $param);
 
-echo clsHttp::curlPost($url, $param);
+$pass = '111111';
+$salt = '888';
+$pass1 = crypt($pass, $salt);
+echo $pass1; // 88.h3CWSFO.yw
