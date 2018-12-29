535de22f9981c3809117c8fe166d6cd0399a77de
hxl <18301805881@163.com>
2018-12-29 10:43:28 +0800

    #update

---
+++ server/sql/test.sql
@@ -0,0 +1,39 @@
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('经典斗地主 - 小资场', 2, 1, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('经典斗地主 - 老板场', 2, 2, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('经典斗地主 - 土豪场', 2, 3, 1);
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('欢乐斗地主 - 小资场', 1, 4, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('欢乐斗地主 - 老板场', 1, 5, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('欢乐斗地主 - 土豪场', 1, 6, 1);
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('癞子斗地主 - 小资场', 1, 7, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('癞子斗地主 - 老板场', 1, 8, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('癞子斗地主 - 土豪场', 1, 9, 1);
+
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('炸金花 - 小资场', 1, 10, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('炸金花 - 老板场', 1, 11, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('炸金花 - 土豪场', 1, 12, 1);
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('抢庄牛牛 - 小资场', 1, 13, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('抢庄牛牛 - 老板场', 1, 14, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('抢庄牛牛 - 土豪场', 1, 15, 1);
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('看牌牛牛 - 小资场', 1, 16, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('看牌牛牛 - 老板场', 1, 17, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('看牌牛牛 - 土豪场', 1, 18, 1);
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('德州扑克 - 小资场', 1, 19, 2);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('德州扑克 - 老板场', 1, 20, 2);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('德州扑克土豪场', 1, 21, 2);
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('十三水 - 小资场', 1, 22, 3);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('十三水 - 老板场', 1, 23, 3);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('十三水 - 土豪场', 1, 24, 3);
+
+
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('龙虎斗', 1, 25, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('红黑大战', 1, 26, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('百人牛牛', 1, 27, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('奔驰宝马', 1, 28, 1);
+insert into admin_game(gameName, groupId, gameType, gameStatus) values('百家乐', 1, 29, 2);
